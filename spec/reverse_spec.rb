5df6521f6059a7a144153f0f9aa1f60bfaf46358
PNdunguMaina <ndunguwamaina@gmail.com>
2022-10-22 17:36:54 +0300

    Fix linter errors on reverse_spec.rb test file

--- spec/reverse_spec.rb
+++ spec/reverse_spec.rb
@@ -1,11 +1,11 @@
-require './solver'
+require_relative '../solver'
 
 describe Solver do
-  context 'When testing reverse method' do
-    it 'Should return 1 when num is 0' do
-      reverse = Solver.new
-      message = reverse.reverse('hello')
-      expect(message).to eq 'olleh'
+  context 'When calling reverse method' do
+    it 'Should return the reverse of the word passed' do
+      Solver.send :public, :reverse
+      reverse = Solver.new.reverse('hello')
+      expect reverse.should eql('olleh')
     end
   end
 end
